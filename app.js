4be110f3165be5dd6788ca65de28306caacaec51
Kenneth Pardo <kennethpardo3@gmail.com>
2025-02-27 21:43:15 -0500

    Capturar, Validar, Actualizar y limpiar

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
 // Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+
+function agregarAmigo(){
+
+    let nombreAmigo = document.getElementById('amigo').value;
+
+    if (nombreAmigo.trim() == ''){
+        alert("Por favor, ingrese un nombre válido");
+    }
+    else {
+        amigos.push(nombreAmigo);
+        limpiarCaja();
+        console.log(amigos);
+    }
+}
+
+function limpiarCaja(){
+    document.querySelector('#amigo').value = '';
+}
+
+
+
